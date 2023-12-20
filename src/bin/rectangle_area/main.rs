83c1f0a65ecd1e2c0a0a0a6fb285fe6f24a1f2d8
Bruno Charamba <brunocharamba@Brunos-Air.lan>
2023-12-20 11:56:24 -0300

    added associated functions and the can hold method

--- src/bin/rectangle_area/main.rs
+++ src/bin/rectangle_area/main.rs
@@ -2,8 +2,13 @@ mod structs;
 use crate::structs::Rectangle;
 
 fn main() {
-  let rectangle: Rectangle = Rectangle { width: 30, height: 40 };
+  let rectangle: Rectangle = Rectangle { width: 30, height: 50 };
   let area: u32 = rectangle.calculate_area();
+
+  let rect2: Rectangle = Rectangle { width: 10, height: 40 };
+  let rect3: Rectangle = Rectangle { width: 60, height: 45 };
+
+  let square: Rectangle = Rectangle::create_square(10);
 
   println!("The ractangle area is: {area}");
   println!("");
@@ -13,6 +18,11 @@ fn main() {
   println!("");
   println!("Is width valid? {}", rectangle.is_width_valid());
   println!("");
-  println!("Is height valid? {}", rectangle.is_height_valid())
+  println!("Is height valid? {}", rectangle.is_height_valid());
+  println!("");
+  println!("can rect hold rect2? {}", rectangle.can_hold_rectangle(&rect2));
+  println!("can rect hold rect3? {}", rectangle.can_hold_rectangle(&rect3));
+  println!("");
+  dbg!("this is a square: {}", &square);
 
 }
--- src/bin/rectangle_area/structs.rs
+++ src/bin/rectangle_area/structs.rs
@@ -5,7 +5,9 @@ pub struct Rectangle {
 }
 
 impl Rectangle {
+    pub fn create_square(size: u32) -> Self { Self { width: size, height: size } }
     pub fn calculate_area(&self) -> u32 { self.height * self.width }
     pub fn is_width_valid(&self) -> bool { self.width > 0 }
     pub fn is_height_valid(&self) -> bool { self.height > 0 }
+    pub fn can_hold_rectangle(&self, rect: &Rectangle) -> bool { self.height > rect.height && self.width > rect.width }
 }
